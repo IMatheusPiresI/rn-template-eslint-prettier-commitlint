40dc3396698c9478ac04d679c44694ba861836ca
Matheus Pires de Sousa <matheuspdsousa@gmail.com>
2022-11-12 08:55:48 -0300

    feat: add statusbar default

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import * as S from './styles';
 
 export const Home = () => (
   <S.Container>
+    <StatusBar
+      barStyle="light-content"
+      translucent
+      backgroundColor="transparent"
+    />
+
     <S.Title>Hello World</S.Title>
   </S.Container>
 );
